0867b84bb7da8a230a12674875acd2dadd6c68bf
Hoàng Minh Ngọc <91311915+NgocJQK@users.noreply.github.com>
2022-12-06 15:28:30 +0700

    file bai tap

---
+++ Lab04/AimsProject/src/hust/soict/dsai/aims/store/Store/Store.java
@@ -0,0 +1,35 @@
+//Họ Và Tên: Hoàng Minh Ngọc 20200440
+//Mã lớp TH: 721427
+package hust.soict.dsai.aims.store.Store;
+import java.util.ArrayList;
+
+import hust.soict.dsai.aims.disc.DigitalVideoDisc.DigitalVideoDisc;
+import java.util.ArrayList;
+public class Store {
+	
+	/** The items in store. */
+	ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<DigitalVideoDisc>();
+	
+
+	/**
+	 * Removes the DVD.
+	 *
+	 * @param disc the disc
+	 */
+	public void removeDVD(DigitalVideoDisc disc) {
+		itemsInStore.remove(disc);
+		System.out.println("The disc " + disc.getTitle() + " has been removed");
+	}
+
+	/**
+	 * Adds the digital video disc.
+	 *
+	 * @param disc the disc
+	 */
+	public void addDVD(DigitalVideoDisc disc) {
+		// Nếu số lượng chưa max thì có thể thêm
+			itemsInStore.add(disc);
+			System.out.println("The disc " + disc.getTitle() + " has been added");
+	}
+	
+}
